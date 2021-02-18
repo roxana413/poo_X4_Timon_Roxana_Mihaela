79f05818ca37d7e15e3f3e8875a355ced974636c
roxana413 <roxana.timon@info.uaic.ro>
2021-02-18 09:09:32 +0200

    Am adaugat laboratorul 1

---
+++ lab1/lab1/lab1.cpp
@@ -0,0 +1,55 @@
+// lab1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+
+bool isPrime(int n)
+
+{
+
+    for (int tr = 2; tr < n / 2; tr++)
+
+        if ((n % tr) == 0)
+
+            return false;
+
+    return true;
+
+}
+
+int main()
+
+{
+
+    int n;
+
+    std::cout << "Enter a number:";
+
+    std::cin >> n;
+
+    if (isPrime(n))
+
+        std::cout << n << " is prime !";
+
+    else
+
+        std::cout << n << " is NOT prime !";
+
+    return 0;
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
